fa9b6502654850ba9ebac6310f13f6932919f423
gariandi <32017473+gariandi@users.noreply.github.com>
2020-03-25 21:57:10 -0300

    Add files via upload
    Uma iniciativa Coronavirus Makers BR !

---
+++ respiradorV1.ino
@@ -0,0 +1,37 @@
+int pino_saida = 2;     // atuador ativado pelo pino digital 2
+int valor = 0;
+
+#include <math.h>
+
+float freq = 12; //respiração/min
+double T = lround(60/freq) ; //segundos
+double t = 0;
+long int espera = lround( 1000*T/(2*255) );
+
+
+void setup() {
+  pinMode(pino_saida, OUTPUT);  // configura  o pino como saída
+  Serial.begin(9600); //vamos ver se sai tudo certo por aqui
+}
+
+void loop() {
+
+  for(int cont=0 ; cont<=255 ; cont++){
+
+      t = cont*T/(2*255) ;  
+      valor = lround(255*sin(2*PI*t/T)); //aqui pode ficar t e T em segundos mesmo
+
+      if( cont % 2 != 0 ){
+        analogWrite(pino_saida,valor);
+        Serial.print("valor = ");
+        Serial.print(valor);
+        delay(espera);
+      }
+      else{
+        delay(2*espera);
+      } 
+    
+    }
+    
+  delay(lround(1000*T/2));
+}
