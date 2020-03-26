6be37c1555e87365f00d0db6e384ce0e160f51e8
gariandi <32017473+gariandi@users.noreply.github.com>
2020-03-26 17:28:05 -0300

    Add files via upload
    Esta linha de trabalho é para automação de fluxo hospitalar, segue o projeto do vídeo:
    
    https://www.youtube.com/watch?v=NNr36bboLXE
    
    As saídas digitais do arduíno precisam ser amplificadas com transistores ou shields para acionamento das eletroválvulas 12V ou 24V. No caso de transistores recomenda-se usar múltiplos elementos em paralelo, compondo uma redundância para evitar falhas.

---
+++ respiradorV2.ino
@@ -0,0 +1,30 @@
+//26-03-2020
+// Essa versão considera o acionamento de 2 válvulas pneumáticas start/stop para administrar o fluxo de ar de uma linha hospitalar
+// Teremos uma válvula de admissão e outra de exaustão
+
+#include <math.h>
+
+const int pino_valv1 = 2;     // atuador ativado pelo pino digital 2
+const int pino_valv2 = 3;
+
+float freq = 10; //respiração/min
+float T = lround(60/freq) ; //segundos
+
+void setup() {
+  pinMode(pino_valv1,OUTPUT);  // configura  o pino 2 como saída da válvula 1
+  pinMode(pino_valv2,OUTPUT);  // configura  o pino 3 como saída da válvula 1
+}
+
+void loop() {
+
+      digitalWrite(pino_valv1,HIGH);
+      digitalWrite(pino_valv2,LOW);
+      
+      delay(lround(1000*T/2)); //milisegundos
+
+      digitalWrite(pino_valv1,LOW);
+      digitalWrite(pino_valv2,HIGH);
+
+      delay(lround(1000*T/2)); //milisegundos
+
+}
